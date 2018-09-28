9171cab8f11fc67051700c859dc252a22cb4aac0
Laithong <40286402+Laithong@users.noreply.github.com>
2018-09-29 05:28:24 +0700

    Add files via upload

---
+++ scan.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   string a;
+   string b;
+   string c;
+   float d;
+   cout << "your name :";
+   cin >>  a;
+   cout << "sername :";
+   cin >>  b;
+   cout << "your email:";
+   cin >>  c;
+   cout << "age:";
+   cin >>  d;
+
+   cout << a<<endl;
+   cout << b<<endl;
+   cout << c<<endl;
+  cout << d<<endl;
+
+   return 0;
+}
